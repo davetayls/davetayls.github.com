73f1dfbb31f326fd9900b822b8b0470e87efe9c2
Dave Taylor <dave@the-taylors.org>
2010-07-01 09:45:01 +0100

    update concepts

--- concepts/yotel/yotel.js
+++ concepts/yotel/yotel.js
@@ -17,13 +17,42 @@
 				if (b){
 					flyout$
 						.animate({
-							'right': -430
+							'right': -570
 						})					
 						.animate({
 							'right': 0
 						});					
+				}else{
+					flyout$
+					.animate({
+						'right': -570
+					});					
 				}
 			};
+			var openPodNav = function(b){
+				if (b){
+					this$.addClass('xnav-t4');
+					$('.xnav-showPOD').addClass('xnav-open');
+				}else{
+					this$.removeClass('xnav-t4');
+					$('.xnav-showPOD').removeClass('xnav-open');
+				}
+			};
+			$('#flyout-close').click(function(){showFlyout(false);return false;});
+			$('.flyout-content a').click(function(){
+				$('.flyout-content a').hide();
+				var this$ = $(this);
+				var next$ = $(this$.attr('href'));
+				/*
+				if (next$.hasClass('xnav-showPOD')){
+					openPodNav(true);
+					showFlyout(false);
+				}else{
+					next$.show();
+				}*/
+				next$.show();
+				return false;
+			});
 			this$.find('li').hover(function(){
 				var tier = $(this).closest('li').parentsUntil('#xnav > ul').length;
 				this$
@@ -47,6 +76,8 @@
 				this$.removeClass('xnav-anchorRight');
 				this$.removeClass('xnav-anchorBottom');
 				
+				openPodNav(false);
+				
 				if (isOpen()){
 					this$.fadeOut(100);
 					return;
@@ -56,9 +87,9 @@
 					left: e.pageX+'px',
 					top: e.pageY + 'px'						
 				};
-				if (e.pageX > (scrWidth-300)){
-					cssLocation.left = 'auto';
-					cssLocation.right = (scrWidth - e.pageX) + 'px';
+				if (e.pageX > (scrWidth-700)){
+					cssLocation.left = (scrWidth-700) + 'px';
+					//cssLocation.right = (scrWidth - e.pageX) + 'px';
 					this$.addClass('xnav-anchorRight'); 
 				}
 				if (scrHeight > 300 && e.pageY > (scrHeight - 300)){
