7d81462627ad900faf1767606e8c2140e23685bc
davetayls <dave@the-taylors.org>
2012-01-17 20:40:00 +0000

    spring cleaning

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/*jslint browser: true, vars: true, white: true, forin: true, nomen: true */
+/*global define,require */
+/**
+* Main
+* ====
+* This is the main js initialiser for the page
+* it is triggered by the data-main attribute on the
+* require script tag.
+* <script data-main="/js/main" src="/lib/require.js"></script>
+* for more information see <http://requirejs.org>
+*/
+/*jslint browser: true, vars: true, white: true, forin: true */
+/*global define,require */
+require(
+{ 
+    paths: { 
+        jquery: '../lib/jquery.min',
+        lib: '../lib', 
+        'jquery-plugin': '../lib/jquery-plugins' 
+    } 
+}, 
+[ 
+    'jquery',
+    'lib/jquery.tweet',
+    'lib/jquery.pager'
+], 
+function($) {
+    'use strict';
+    
+    $('pre>code').parent().addClass('prettyprint');
+    window.prettyPrint();
+
+    $('#tweets').tweet({
+        username: 'davetayls',
+        avatar_size: 32
+    });
+
+    $('.list-blogs').pager({
+        pageSize: 5,
+        pagerLocation: "before"
+    });
+
+});
+
--- scripts/keys.js
+++
@@ -1,7 +0,0 @@
-// key codes
-/*global define */
-define({
-    ENTER:  13,
-    ESC:    27,
-    TAB:    9
-});
