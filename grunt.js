c134373162a05baf4a2c14237bfbdc80528f5b37
davetayls <dave@the-taylors.org>
2013-08-04 08:21:34 +0100

    teakies

--- grunt.js
+++ grunt.js
@@ -19,6 +19,7 @@ module.exports = function(grunt) {
           paths: [
             '_stylus',
             '_stylus/components',
+            '_stylus/blocks',
             'lib',
             'node_modules/nib/lib'
           ]
--- js/main.js
+++ js/main.js
@@ -1,26 +1,12 @@
-/*jslint browser: true, vars: true, white: true, forin: true, nomen: true */
-/*global define,require */
-/**
-* Main
-* ====
-* This is the main js initialiser for the page
-* it is triggered by the data-main attribute on the
-* require script tag.
-* <script data-main="/js/main" src="/lib/require.js"></script>
-* for more information see <http://requirejs.org>
-*/
-/*global define,require */
 require(
 {
     paths: {
         jquery: '../lib/jquery.min',
-        lib: '../lib',
-        'jquery-plugin': '../lib/jquery-plugins'
+        lib: '../lib'
     }
 },
 [
     'jquery',
-    'lib/jquery.tweet',
     'lib/jquery.pager'
 ],
 function ($) {
@@ -31,11 +17,6 @@ function ($) {
 
     $('pre>code').parent().addClass('prettyprint');
     window.prettyPrint();
-
-    $('#tweets').tweet({
-        username: 'davetayls',
-        avatar_size: 32
-    });
 
     $('.list-blogs').pager({
         pageSize: 5,
